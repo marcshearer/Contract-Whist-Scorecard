d38c060fd7bd9a561ab83e2393b4acc4e37be5cc
Marc Shearer <marc.shearer@tesco.net>
2019-07-04 05:46:04 +0100

    Backup before color rename

--- Contract Whist Scorecard/Scorecard UI.swift
+++ Contract Whist Scorecard/Scorecard UI.swift
@@ -10,46 +10,6 @@ import UIKit
 
 class ScorecardUI {
     
-    static let backgroundColor = UIColor(named: "Background")!
-    static let bannerColor = UIColor(named: "Banner")!
-    static let bannerTextColor = UIColor(named: "Banner Text")!
-    static let boldColor = UIColor(named: "Bold")!
-    static let boldTextColor = UIColor(named: "Bold Text")!
-    static let darkHighlightColor = UIColor(named: "Dark Highlight")!
-    static let darkHighlightTextColor = UIColor(named: "Dark Highlight Text")!
-    static let emphasisColor = UIColor(named: "Emphasis")!
-    static let emphasisTextColor = UIColor(named: "Emphasis Text")!
-    static let errorColor = UIColor(named: "Error")!
-    static let errorTextColor = UIColor(named: "Error Text")!
-    static let handColor = UIColor(named: "Hand")!
-    static let handTextColor = UIColor(named: "Hand Text")!
-    static let highlightColor = UIColor(named: "Highlight")!
-    static let highlightTextColor = UIColor(named: "Highlight Text")!
-    static let madeContractColor = UIColor(named: "Made Contract")!
-    static let madeContractTextColor = UIColor(named: "Made Contract Text")!
-    static let sectionHeadingColor = UIColor(named: "Section Heading")!
-    static let sectionHeadingTextColor = UIColor(named: "Section Heading Text")!
-    static let tableTopColor = UIColor(named: "Table Top")!
-    static let tableTopTextColor = UIColor(named: "Table Top Text")!
-    static let totalColor = UIColor(named: "Total")!
-    static let totalTextColor = UIColor(named: "Total Text")!
-    static let thumbnailDiscColor = UIColor(named: "Thumbnail Disc")!
-    static let thumbnailDiscTextColor = UIColor(named: "Thumbnail Disc Text")!
-    static let textColor = UIColor(named: "Text")!
-    static let textEmphasisColor = UIColor(named: "Text Emphasis")!
-    static let textErrorColor = UIColor(named: "Text Error")!
-    static let textMessageColor = UIColor(named: "Text Message")!
-    static let shapeStrokeColor = UIColor(named: "Shape Stroke")!
-    static let shapeFillColor = UIColor(named: "Shape Fill")!
-    static let shapeHighlightStrokeColor = UIColor(named: "Shape Highlight Stroke")!
-    static let shapeHighlightFillColor = UIColor(named: "Shape Highlight Fill")!
-    static let suitDiamondsHeartsColor = UIColor(named: "Suit Diamonds Hearts")!
-    static let suitClubsSpadesColor = UIColor(named: "Suit Clubs Spades")!
-    static let suitNoTrumpsColor = UIColor(named: "Suit No Trumps")!
-    static let contractOverColor = UIColor(named: "Contract Over")!
-    static let contractUnderColor = UIColor(named: "Contract Under")!
-    static let contractEqualColor = UIColor(named: "Contract Equal")!
-
     class func roundCorners(_ view: UIView, percent: CGFloat = 0.0) {
         if percent == 0.0 {
             view.layer.cornerRadius = 5

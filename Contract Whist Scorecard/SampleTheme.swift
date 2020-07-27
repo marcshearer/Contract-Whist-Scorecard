e49ef8adf432d640afe7ce2f24253a6751597afe
Marc Shearer <marc@sheareronline.com>
2020-07-27 11:39:35 +0100

    Change colour theme selection carousel to simpler view

--- Contract Whist Scorecard/SampleTheme.swift
+++ Contract Whist Scorecard/SampleTheme.swift
@@ -12,9 +12,6 @@ class SampleTheme: UIView {
 
     @IBOutlet private weak var banner: UIView!
     @IBOutlet private weak var bannerLabel: UILabel!
-    @IBOutlet private weak var titleBar: TitleBar!
-    @IBOutlet private weak var leftButton: ImageButton!
-    @IBOutlet private weak var rightButton: ImageButton!
     @IBOutlet private weak var contentView: UIView!
     
     override init(frame: CGRect) {
@@ -32,8 +29,6 @@ class SampleTheme: UIView {
         self.addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.leftButton.isEnabled = false
-        self.rightButton.isEnabled = false
         self.layoutSubviews()
         self.setColors()
     }
@@ -45,18 +40,5 @@ class SampleTheme: UIView {
         self.banner?.backgroundColor = theme.color(.banner, .current)
         self.bannerLabel?.textColor = theme.color(.bannerText, .current)
         self.contentView?.backgroundColor = theme.color(.background, .current)
-        self.titleBar?.set(faceColor: theme.color(.buttonFace, .current))
-        self.titleBar?.set(textColor: theme.color(.buttonFaceText, .current))
-        self.titleBar?.set(font: UIFont.systemFont(ofSize: 5, weight: .bold))
-        self.leftButton?.set(faceColor: theme.color(.buttonFace, .current))
-        self.leftButton?.set(titleColor: theme.color(.buttonFaceText, .current))
-        self.leftButton?.set(titleFont: UIFont.systemFont(ofSize: 5))
-        self.leftButton?.set(imageTintColor: theme.color(.banner, .current))
-        self.leftButton?.setProportions(top: 1, image: 2, imageBottom: 1, title: 1, titleBottom: 0, message: 0, bottom: 1)
-        self.rightButton?.set(faceColor: theme.color(.buttonFace, .current))
-        self.rightButton?.set(titleColor: theme.color(.buttonFaceText, .current))
-        self.rightButton?.set(titleFont: UIFont.systemFont(ofSize: 5))
-        self.rightButton?.set(imageTintColor: theme.color(.banner, .current))
-        self.rightButton?.setProportions(top: 1, image: 2, imageBottom: 1, title: 1, titleBottom: 0, message: 0, bottom: 1)
     }
 }
